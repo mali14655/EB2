f78e21f6ae004bea13a8a36d5e999cb022923b00
Muhammad Ali <mali146643@gmail.com>
2024-11-13 12:48:15 +0500

    new2

--- Backend/api/server.js
+++ Backend/api/server.js
@@ -12,8 +12,9 @@ const port = 3010; // You can change the port if needed
 
 app.use(cors({
   origin: "https://eb-2.vercel.app/"
-  // origin: "http://localhost:5175"
-})); // Allow all origins (or restrict it to specific ones)
+  // origin: "http://localhost:5173"
+})); 
+// Allow all origins (or restrict it to specific ones)
 // Middleware to parse JSON bodies
 app.use(express.json());
 
